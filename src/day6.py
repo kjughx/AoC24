aa8977ccd6be59fcba15614ce70ce681176f30b0
Philip J. <kjughx@gmail.com>
2024-12-06 07:33:14 +0100

    Day 6, python took so long I had time to write p2 in rust...

---
+++ src/day6.py
@@ -0,0 +1,57 @@
+#!/bin/env python3
+import numpy as np
+from copy import deepcopy
+
+
+def inbounds(rc):
+    if rc[0, 0] < 0 or rc[0, 0] >= R:
+        return False
+
+    if rc[1, 0] < 0 or rc[1, 0] >= R:
+        return  False
+
+    return True
+
+def key(rc, drdc):
+    return tuple(map(int,(rc[0, 0], rc[1, 0], drdc[0,0], drdc[1,0])))
+
+def walk(rc, grid):
+    drdc = np.array([-1, 0])
+    drdc.shape = (2, 1)
+    rot = np.matrix([[0, 1], [-1,0]])
+    vis = set()
+    vis.add(key(rc, drdc))
+    while True:
+        nrc = rc + drdc
+        if not inbounds(nrc):
+            break
+        nkey = key(nrc, drdc)
+        if nkey in vis: # loop
+            return False, vis
+        vis.add(key(nrc, drdc))
+
+        if grid[nrc[0, 0], nrc[1, 0]] != '#':
+            rc = nrc
+            continue
+
+        drdc = rot * drdc
+    return True, vis
+
+
+with open('inputs/day6') as file:
+    grid = np.matrix([[a for a in row.strip('\n')] for row in file.readlines()])
+
+    rc = np.array(np.where(grid == "^"))[:, 0]
+    rc.shape = (2, 1)
+    R, C = np.shape(grid)
+    grid[rc[0],rc[0]]='.'
+
+    print(len(walk(rc, grid)[1]) - 4) # part 1
+
+    loops = 0
+    for i in range(np.shape(grid)[0]):
+        for j in range(np.shape(grid)[1]):
+            ngrid = deepcopy(grid)
+            ngrid[i, j] = '#'
+            loops += not walk(rc, ngrid)[0]
+    print(loops)
---
+++ src/day6/src/main.rs
@@ -0,0 +1,62 @@
+use std::collections::HashSet;
+
+fn walk(mut r: isize, mut c: isize, grid: Vec<Vec<char>>) -> bool {
+    let (mut dr, mut dc) = (-1, 0);
+    let (R, C) = (grid.len(), grid[0].len());
+    let mut vis: HashSet<(isize, isize, isize, isize)> = HashSet::new();
+    vis.insert((r, c, dr, dc));
+
+    loop {
+        let (nr, nc) = ((r as isize + dr), (c as isize + dc));
+        if nr < 0 || nc < 0 || nr >= R as isize || nc >= C as isize {
+            break;
+        }
+        let key = (nr, nc, dr, dc);
+        if vis.contains(&key)  {
+            return false;
+        }
+
+        if grid[nr as usize][nc as usize] != '#' {
+            (r, c) = (nr, nc);
+            vis.insert(key);
+        } else{
+            (dr, dc) = (dc, -dr);
+        }
+    }
+    
+    return true;
+}
+
+fn main() {
+    let input = std::fs::read_to_string("../AoC24/inputs/day6").unwrap();
+
+    let grid: Vec<Vec<char>> = input
+        .trim()
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let (mut r, mut c) = (0, 0);
+    'll: for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if grid[i][j] == '^' {
+                (r, c) = (i, j);
+                break 'll;
+            }
+    }}
+
+    let mut loops = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            let mut ngrid = grid.clone();
+            ngrid[i][j] = '#';
+            if !walk(r as isize, c as isize, ngrid) {
+                loops += 1;
+            }
+
+        }
+    }
+    println!("{loops}");
+
+}
