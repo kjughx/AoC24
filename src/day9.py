fdf51f086cf0b40db3b9cc374bd1db6934d7caef
Philip J. <kjughx@gmail.com>
2024-12-09 08:42:12 +0100

    Day9, part 2 incomplete

--- src/day9.py
+++ src/day9.py
@@ -1,6 +1,22 @@
 #!/bin/env python3
 from copy import deepcopy
 
+def chunk(f, l):
+    c = []
+    for i in f:
+        if not c:
+            c.append(i)
+            continue
+
+        if i == c[0]:
+            c.append(i)
+        else:
+            if l == -1 or len(c) <= l:
+                break
+            else:
+                c = [i]
+    return c, len(c)
+
 with open('inputs/day9') as file:
     disk = list(map(int, file.readline().strip()))
     counts = deepcopy(disk)
@@ -16,19 +32,40 @@ with open('inputs/day9') as file:
 
     back = len(inter1)
     front = 0
+    chunks = {}
+    while front <= len(inter1):
+        c, l = chunk(inter1[front:], -1)
+        front += l
+        if not c:
+            break
+        chunks[c[0]] = c
+
+    real = []
     for i, d in enumerate(disk):
         if i % 2 == 0:
-            front += d
-        else:
-            inter[front:front+d] = reversed(inter1[back - d: back])
-            back -= d
-            front += d
-
-        if '.' not in inter:
-            inter = inter[:len(inter1)]
-            break
-
-    for i, d in enumerate(inter):
-        cs += i * d
-
+            print(i, chunks[i // 2])
+        # else:
+    # print(real)
+    # for i, d in enumerate(disk):
+    #     if i % 2 == 0:
+    #         front += d
+    #     else:
+    #         while d >= 0:
+    #             c,l = chunk(reversed(inter1[front:back]), d)
+    #             if c:
+    #                 inter[front:front+l] = c
+    #                 back -= l
+    #                 d -= l
+    #                 front += l
+    #             else:
+    #                 break
+    #
+    #     if '.' not in inter:
+    #         inter = inter[:len(inter1)]
+    #         break
+    #
+    # print(inter)
+    # for i, d in enumerate(inter):
+    #     cs += i * d
+    #
     print(cs)
