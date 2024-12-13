5e471b8f1f4f7584c64c0ffef07eda05375e0734
Philip J. <kjughx@gmail.com>
2024-12-13 06:32:06 +0100

    Day 13, math ftw

---
+++ src/day13.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+import re
+
+with open('inputs/day13') as file:
+    p = 0
+
+    lines = [line for line in file.readlines()]
+
+    part2 = False
+
+    line = 1
+    for l in range(0, len(lines), 4):
+        ax, ay = list(map(int, re.findall(r"Button A: X\+([0-9]+), Y\+([0-9]+)", lines[l])[0]))
+        bx, by = list(map(int, re.findall(r"Button B: X\+([0-9]+), Y\+([0-9]+)", lines[l + 1])[0]))
+        px, py = list(map(int, re.findall(r"Prize: X=([0-9]+), Y=([0-9]+)", lines[l + 2])[0]))
+
+        if part2:
+            px += 10000000000000
+            py += 10000000000000
+
+        b = (py * ax - px * ay) / (ax * by - bx * ay)
+        a = ( px - b * bx)  / ax
+
+        if a.is_integer() and b.is_integer():
+            p += 3 * a + b
+
+    print(p)
