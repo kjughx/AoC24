1cce06451f4937dbb1008fdb1b611e21bcbb1a21
Philip Johansson <philjn@axis.com>
2024-12-20 14:53:25 +0100

    Day 20, part 2 not done - insane
    Change-Id: I2286d26f2b7b7971fab90b8dd6b69ffc9e25569c

--- src/day20.py
+++ src/day20.py
@@ -2,36 +2,13 @@
 from collections import deque
 import numpy as np
 
-# pylint: disable=C0114,C0116,C0301,C0209,W1514,C0414,C0200,E0001
+# pylsp: disable=E302,C0114,C0116,C0301,C0209,W1514,C0414,C0200,E0001
 
 grid = np.array([[c for c in row.strip('\n')] for row in open(0).readlines()])
 R, C = np.shape(grid)
-print(R, C)
 
 walls = np.where(grid == '#')
-walls = [(r, c) for r,c in zip(walls[0], walls[1])]
-
-jable = []
-
-
-"""
-wall (r,c) is jumpable if it's the only wall in [(r+dr,c+dc),(r,c),(r-dr, c-dc)]
-"""
-
-jable = {}
-for (r,c) in walls:
-	for dr,dc in [(0,1), (1,0)]:
-		br, bc = r - dr, c - dc
-		nr, nc = r + dr, c + dc
-		if not (0 <= r - dr < R and 0 <= c - dc < C and 0 <= r + dr < R and 0 <= c + dc < C):
-			continue
-		if [grid[r-dr,c-dc], grid[r, c], grid[r+dr,c+dc]].count('#') == 1:
-			if (r - dr, c - dc) not in jable:
-				jable[(r - dr, c - dc)] = []
-			if (r + dr, c + dc) not in jable:
-				jable[(r + dr, c + dc)] = []
-			jable[(r - dr, c - dc)].append((r + dr, c + dc))
-			jable[(r + dr, c + dc)].append((r - dr, c - dc))
+walls = [(r, c) for r, c in zip(walls[0], walls[1])]
 
 sr, sc = np.where(grid == 'S')
 fr, fc = np.where(grid == 'E')
@@ -49,53 +26,116 @@ T = R * C
 vis = {}
 path = []
 while q:
-	r, c, t, p = q.popleft()
-	if (r, c) == (fr, fc):
-		T = min(T, t)
-		path = p
+    r, c, t, p = q.popleft()
+    if (r, c) == (fr, fc):
+        T = min(T, t)
+        path = p
 
-	if (r, c) in vis and vis[(r, c)] < t:
-		continue
-	vis[(r, c)] = t
-	
-	for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-		nr, nc = r+dr, c+dc
-		if 0 <= nr < R and 0 <= nc < C and grid[nr, nc] != '#':
-			q.append((nr, nc, t + 1, p + [(nr, nc)]))
+    if (r, c) in vis and vis[(r, c)] < t:
+        continue
+    vis[(r, c)] = t
+
+    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        nr, nc = r+dr, c+dc
+        if 0 <= nr < R and 0 <= nc < C and grid[nr, nc] != '#':
+            q.append((nr, nc, t + 1, p + [(nr, nc)]))
+
 OT = T
 
-TS = [0 for _ in range(len(path) + 1)]
-q = deque([(sr, sc, 0, False)])
+
 vis = {}
-#while q:
-#	r, c, t, has_cheated = q.popleft()
-#	if (r, c) == (fr, fc):
-#		print(t)
-#		TS[t] += 1
-#		continue
-#	
-#	for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-#		nr, nc = r+dr, c+dc
-#		if 0 <= nr < R and 0 <= nc < C:
-#			if (nr, nc) in jable and not has_cheated:
-#				if (nr + dr, nc + dc) == '.':
-#					if path.index((nr + dr, nc + dc)) > path.index((r, c)):
-#						q.append((nr + dr, nc + dc, t + 2, True))
-#			elif grid[nr, nc] != '#':
-#				q.append((nr, nc, t + 1, False))
+
+
+HAS_CHEATED = 1
+IS_CHEATING = 2
+
+cheats = {}
+
+
+def reachable1(rr, cc, t, cheating):
+    if t == CT + 1:
+        return set()
+
+    if (rr, cc, t) in vis:
+        return vis[(rr, cc, t)]
+
+    ans = set()
+
+    if grid[rr, cc] == '.':  # Then this is done
+        ot = len(path[:path.index((rr, cc))])
+        if t < ot:
+            ans.add((rr, cc, t))
+
+    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        nr, nc = rr + dr, cc + dc
+        if not (0 <= nr < R and 0 <= nc < C):
+            continue
+        if grid[nr, nc] == '#':
+            if cheating & IS_CHEATING or cheating ^ HAS_CHEATED:
+                ans |= reachable(nr, nc, t + 1, cheating | IS_CHEATING)
+        else:
+            if cheating & IS_CHEATING:
+                ans |= reachable(nr, nc, t + 1, cheating |
+                                 HAS_CHEATED ^ IS_CHEATING)
+            if not cheating:
+                ans |= reachable(nr, nc, t + 1, cheating)
+
+    vis[(rr, cc, t)] = ans
+
+    return ans
+
+
+# def reachable(r, c, t):
+#     ans = set()
+#
+#     for dr in range(-CT, CT + 1):
+#         for dc in range(-CT, CT + 1):
+#             if abs(dr) + abs(dc) > CT:
+#                 continue
+#             nr, nc = r + dr, c + dc
+#             if 0 <= nr < R and 0 <= nr < C and grid[nr, nc] == '.':
+#                 if path.index((nr, nc)) < path.index((r, c)):
+#                     continue
+#                 if path.index((nr, nc)) - path.index((r, c)) >= MC:
+#                     CC += 1
+#     return ans
 #
 
-def adjecent(r, c, nr, nc):
-	return abs(nr - r) == 1 or abs(nc - c) == 1
+vis = {}
 
-for r,c in path: 
-	if (r, c) not in jable:
-		continue
 
-	for jr, jc in jable[(r, c)]:
-		if path.index((jr, jc)) < path.index((r, c)):
-			continue
-		t = len(path[:path.index((r, c))]) + len(path[path.index((jr, jc)):])
-		TS[OT - t] += 1
+def reachable(r, c, t):
+    if t == CT + 1:
+        return set()
 
-print(np.sum(TS[100:]))
+    ans = set()
+
+    if (r, c, t) in vis:
+        return vis[(r, c, t)]
+
+    if grid[r, c] == '.':
+        ans.add((r, c, t))
+
+    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < R and 0 <= nc < C:
+            ans |= reachable(nr, nc, t + 1)
+
+    vis[(r, c, t)] = ans
+
+    return ans
+
+
+CT = 6
+MC = 0
+CC = 0
+TS = [0 for _ in range(OT)]
+for r, c in path:
+    for jr, jc, ts in reachable(r, c, 0):
+        if path.index((jr, jc)) < path.index((r, c)):
+            continue
+
+        t = len(path[:path.index((r, c))]) + len(path[path.index((jr, jc)):])
+        TS[OT - t] += 1
+
+print(TS)
