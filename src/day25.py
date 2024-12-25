9dc57cd94f6caaf09b345826893a89a7340033d9
Philip J. <kjughx@gmail.com>
2024-12-25 12:06:52 +0100

    Day 25, nice

---
+++ src/day25.py
@@ -0,0 +1,27 @@
+#!/bin/env python3
+import numpy as np
+
+# pylint: disable=C0114,C0116,C0301,C0209,W1514,C0414,C0200,E0001
+
+lines = open(0).read()[:-1]
+
+keys = []
+locks = []
+
+for t in lines.split('\n\n'):
+    t = np.array([[1 if c == '#' else 0 for c in row] for row in t.split('\n')])
+    if np.all(t[0, :] == 1):
+        locks.append(t)
+    else:
+        keys.append(t)
+
+V = 35
+p = 0
+for lock in locks:
+    for key in keys:
+        fit = lock + key
+        if np.all(fit <= 1):
+            p += 1
+
+print(p)
+
